de6c4abfe01fa7db036f781d50c9cb1e8a0aa373
pengbits <pengbits@gmail.com>
2025-06-26 16:52:26 -0400

    loading api from 5000 with proxy config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,16 @@
 import './App.css'
-
+import { useEffect } from 'react'
 function App() {
+  
+  const loadGreeting = async () => {
+    const res = await fetch('/api')
+    const json = await res.json()
+    console.log(json.greeting, '')
+  }
+
+  useEffect(() => {
+    loadGreeting()
+  }, [])
 
   return (
     <>
