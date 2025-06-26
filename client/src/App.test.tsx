c3862a5bc977f287ef3b0f25e4d7dc35e5ad3916
pengbits <pengbits@gmail.com>
2025-06-26 16:33:12 -0400

    vitest set up, but jest extension for dom matchers not working

---
+++ client/src/App.test.tsx
@@ -0,0 +1,12 @@
+import {describe,it,expect} from 'vitest'
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('Renders <App />', async () => {
+    render(<App />)
+
+    const h1 = await screen.findByText('Ahoy')
+    console.log(h1)
+  })
+})
