a4eae13a5a7a59ae556fa62eb29e7f0c6fa967db
pengbits <pengbits@gmail.com>
2025-06-28 16:20:56 -0400

    updates to routes to reflect radius in meters, not km

--- server/src/routes/meters.test.ts
+++ server/src/routes/meters.test.ts
@@ -5,7 +5,7 @@ import app from "../app"
 describe('meters', () => {
   describe('GET /api/meters/lat,long/radius', () => {
     it('returns an array of meters that fall in the search area', async () => {
-      const response = await request(app).get('/api/meters/40.645344,-73.9517345/200')
+      const response = await request(app).get('/api/meters/40.645344,-73.9617345/1')
       expect(response.status).toBe(200)
       expect(response.body.meters).toBeTruthy()
       expect(response.body.meters).toEqual(expect.arrayContaining([{
--- server/src/routes/meters.ts
+++ server/src/routes/meters.ts
@@ -3,8 +3,8 @@ import GetMetersMock from '../mocks/GetMeters';
 const router = express.Router();
 
 router.get('/:lat,:long/:radius', (req:Request,res:Response) => {
-  const {lat,long,radius} = req.params
-  console.log(`find meters that fall within this search area:`, lat,long,radius)
+  const {lat,long,radius} = req.params // radius is in meters
+  console.log(`find parking meters that fall within this search area:`, lat,long,radius)
   res.json({
     meters:GetMetersMock
   })
