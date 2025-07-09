eae6891a12019f5271d823a3f69a7466478d07c9
pengbits <pengbits@gmail.com>
2025-07-09 15:54:11 -0400

    clearinf properly with useRef

--- client/src/components/sessions/CountDownWidget.tsx
+++ client/src/components/sessions/CountDownWidget.tsx
@@ -1,4 +1,4 @@
-import { useState , useEffect} from "react"
+import { useState , useEffect, useRef, RefObject} from "react"
 import {newDate, toDate} from "@/util/date"
 
 interface CountDownWidgetProps  {
@@ -25,8 +25,9 @@ const render = ({hours,mins,seconds}:TimeObject) => {
 
 export const CountDownWidget = ({ends}:CountDownWidgetProps) => {
   let now = newDate()
-  let id:any
-  
+  let id = useRef(0)
+  let diff = useRef(Infinity)
+
   const [time,setTime] = useState({
     hours:0,
     mins:0,
@@ -34,35 +35,32 @@ export const CountDownWidget = ({ends}:CountDownWidgetProps) => {
   } as TimeObject)
 
   const update = () => {
-    let t = {} as TimeObject, remainingHours, remainingSeconds, diff;
+    let t = {} as TimeObject, remainingHours, remainingSeconds;
    
     now = now.add(1, 'second')
-    diff = toDate(ends).diff(now, 'second', true)
+    diff.current = toDate(ends).diff(now, 'second', true)
    
-    t.hours           = Math.floor(diff / 3600)
-    remainingHours    = diff % 3600
+    t.hours           = Math.floor(diff.current / 3600)
+    remainingHours    = diff.current % 3600
     t.mins            = Math.floor(remainingHours / 60)
     remainingSeconds  = remainingHours % 60
     t.seconds         = Math.floor(remainingSeconds)
 
-    console.log(diff)
     setTime(t)
   }
 
   // id needs to be a ref to persist across updates
   useEffect(() => {
-    id = setInterval(update, 1000)
-    return () => (clearInterval(id))
+    id.current = Number(setInterval(update, 1000))
+    return () => (clearInterval(id.current))
   }, [ends])
   
-  if(time.hours > 0 && time.mins > 0 && time.seconds > 0){
+  if(diff.current > 0){
     return render(time)
   } else {
-    // this doesn't work
-    clearInterval(id)
-    return <p>Expired!</p>
+    clearInterval(id.current)
+    return <p>Expired</p>
   }
-  
 }
 
 export default CountDownWidget
