60f624c4fa91b0ed608bb0ae1b7de9a3c92806bb
pengbits <pengbits@gmail.com>
2025-07-09 12:10:09 -0400

    working except for clearing interval when reaching 0

--- client/src/components/sessions/CountDownWidget.tsx
+++ client/src/components/sessions/CountDownWidget.tsx
@@ -1,5 +1,5 @@
 import { useState , useEffect} from "react"
-import {newDate, toDate, Dayjs} from "@/util/date"
+import {newDate, toDate} from "@/util/date"
 
 interface CountDownWidgetProps  {
   ends: string
@@ -26,7 +26,7 @@ const render = ({hours,mins,seconds}:TimeObject) => {
 export const CountDownWidget = ({ends}:CountDownWidgetProps) => {
   let now = newDate()
   let id:any
-
+  
   const [time,setTime] = useState({
     hours:0,
     mins:0,
@@ -45,18 +45,22 @@ export const CountDownWidget = ({ends}:CountDownWidgetProps) => {
     remainingSeconds  = remainingHours % 60
     t.seconds         = Math.floor(remainingSeconds)
 
+    console.log(diff)
     setTime(t)
   }
 
+  // id needs to be a ref to persist across updates
   useEffect(() => {
     id = setInterval(update, 1000)
     return () => (clearInterval(id))
   }, [ends])
   
-  if(time.seconds > 0){
+  if(time.hours > 0 && time.mins > 0 && time.seconds > 0){
     return render(time)
   } else {
+    // this doesn't work
     clearInterval(id)
+    return <p>Expired!</p>
   }
   
 }
