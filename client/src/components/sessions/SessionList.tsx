66f2f668d64cd7ede86aadc7547eb168bcd20f81
pengbits <pengbits@gmail.com>
2025-07-16 15:37:07 -0400

    basic styling of session-list

--- client/src/components/sessions/SessionList.tsx
+++ client/src/components/sessions/SessionList.tsx
@@ -1,11 +1,12 @@
 import type ParkingSessionAttributes from "@/types/api/ParkingSessionAttributes"
 import SessionListItem from "./SessionListItem"
+import "./SessionList.css"
 
 interface SessionListProps {
   sessions : Array<ParkingSessionAttributes>
 }
-const SessionList = ({sessions}:SessionListProps) => {
 
+const SessionList = ({sessions}:SessionListProps) => {
   return (<ul className="parking-sessions">
     {(sessions).map(s => <SessionListItem key={s.id} {...s} />)}
   </ul>)
